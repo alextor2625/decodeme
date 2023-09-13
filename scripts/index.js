6db71f7e93e49206b67c6aa6598c47d53b3f6b5c
alextor26 <alextor2625@gmail.com>
2023-09-12 22:05:19 -0400

    Slider Branch Created

--- scripts/index.js
+++ scripts/index.js
@@ -1,67 +1,25 @@
-
-let randomKey = Math.floor(Math.random()*6)+1;
-let game = new DecodeMeGame(randomKey);
-
-function newLevel(){
-    let encWord = document.getElementById('enc-word');
-    let key = document.getElementById('key');
-    let playerInput = document.getElementById('player-input')
-    encWord.innerHTML = game.getCurrEncWord()
-    game.randomKey(10);
-    key.innerHTML = game.getCurrKey();
-    playerInput.value = '';
-    console.log(game.dictionary.decDict[game.getCurrEncWord()]);
-
-}
-
 window.onload = () => {
-    let startButton = document.getElementById('start-button');
-    startButton.addEventListener('click', ()=>{
-        console.log("Starting Game");
-        game.gameStart();
-        console.log(game.dictionary.encDict);
-        
-        
-        let html = '';
-            html += `
-            <div id="game-level">
-                <div class="content">
-                    <p id="enc-word-prompt">Encrypted Word: <span id="enc-word">${game.getCurrEncWord()}</span></p>
-                    <p id="key-word-prompt">Key: <span id="key">${game.getCurrKey()}</span></p>
-                </div>
-                <div class="button"><span>></span>
-                    <input type="text" id="player-input"></input>
-                    <button id="submit-button">Submit</button>
-                </div>
-                <div class="cipher-slider"></div>
-            </div>
-            `
-                    
-                    
-            document.querySelector("#game-content").innerHTML = html;
-            let input = document.getElementById('player-input')
-            console.log(game.dictionary.decDict[game.getCurrEncWord()]);
-            let button = document.getElementById('submit-button');
-            button.addEventListener('click',  () => {
-                if(game.scoring(input.value)){
-                    newLevel();
-                }
-            })
-            input.onchange = (e) =>{
-                console.log(e.target.value);
-                        
-            }
-        
-        game.startTimerCountdown();
 
+    let slider = document.getElementById('sub-alpha')
+    let position = -500
+    slider.style.left = `${position}px`
+
+    document.addEventListener('keydown', (e) => {
+        console.log("Event", e)
+
+        // let position = slider.getAttribute('style')
+        console.log("position", position)
+
+        switch (e.key) {
+            case 'ArrowLeft':
+                position -= 20
+                slider.style.left = `${position}px`
+                console.log(position)
+                break
+            case "ArrowRight":
+                position += 20
+                slider.style.left = `${position}px`
+                console.log(position)
+        }
     })
-    // console.log('THIS IS THE END', ' ', game.isGameOver());
-    // if(game.isGameOver()){
-    //     showGameEndScreen()
-    // }
-                
-
 }
-
-
-// console.log(new Dictionary(1).caesarDecrypt(1))
